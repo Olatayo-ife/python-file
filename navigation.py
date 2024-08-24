f7d12bc68201d4892d2753091a822ce43c4b9239
Olatayo Hezekiah Love <104759032+Olatayo-ife@users.noreply.github.com>
2024-08-24 08:58:01 -0700

    Add files via upload

---
+++ navigation.py
@@ -0,0 +1,42 @@
+ # Lets store a favourite number
+# Favourite_Number = 73
+# print(f"My favourite number is: {Favourite_Number}")
+
+# # string
+# greeting = "Hello, PLP Academy"
+# print(greeting)
+
+
+# #Boolen
+# is_python_fun = True
+# is_python_boring = False
+# print(f"Is Python fun: {is_python_fun}")
+# print(f"Is python Boring? {is_python_boring}") 
+
+
+# #Control_flow
+# temperature = 30
+# if temperature > 25:
+#     print("Its hot outside! Wear Shorts.") 
+# elif temperature > 15:
+#    print("Its warm. Maybe wear a light jacket")
+# else:
+#     print("Brrrrhh! Its cold. Bundle up!")
+    
+#Modules
+# #In-built Module
+# import math
+# result = math.sqrt(144)
+# print(f"The square root of 144 is: {result}")
+
+#Customize module
+import greeting
+
+
+print(greeting.greet("Mark"))
+print(greeting.greet("Olatayo"))
+
+
+  
+
+
